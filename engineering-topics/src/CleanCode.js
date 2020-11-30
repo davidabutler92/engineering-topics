f8d80763d223fa97d18c08b496ec4f26e2c808ac
davidabutler92 <davidabutler92@yahoo.com>
2020-11-29 16:49:03 -0800

    add cleancode

--- engineering-topics/src/CleanCode.js
+++ engineering-topics/src/CleanCode.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import image from './cleancode-3.png';
 
 export default class CleanCode extends Component {
     render() {
         return (
-            <div>
+            <div className='clean-code-div'>
                 <h1>Clean Code chapter-1</h1>
+                <div className='cleanCode-quote'>
+                    <p className='italics'>"They had rushed the product to
+                    market and had made a huge mess in the code. As they added more and more features, the
+                    code got worse and worse until they simply could not manage it any longer. 
+                    It was the bad code that brought the company down."</p>
+                    <p>This is a good example of bad code. Wanting new features is great and all but you need to have a gmae plan at tackling the code, managing the code, and then producing more.
+                        Having clear consice code and all around readable code makes it easier to pass the project down to others.
+                    </p>
+                </div>
+                <div>
+                    <p className='italics'>"I like my code to be elegant and efficient. The
+                    logic should be straightforward to make it hard
+                    for bugs to hide, the dependencies minimal to
+                    ease maintenance, error handling complete
+                    according to an articulated strategy, and performance close to optimal so as not to tempt
+                    people to make the code messy with unprincipled optimizations. Clean code does one thing
+                    well."
+                    </p>
+                    <p>This is a great quote by the author of c++ Programming Language, Bjarne Stroustrup. 
+                        Elegant and pleasing are great words to use when talking about writing good code. 
+                        It really can be an art and it does take a lot of practice BUT in the long run it saves time for whoever works on your code.</p>
+                </div>
+                <img src={image} alt='clean vs bad code'/>Clean VS bad code
                 <Link to='/'>Home page</Link>
             </div>
         )
--- engineering-topics/src/Quantity.js
+++ engineering-topics/src/Quantity.js
@@ -6,8 +6,18 @@ export default class HomePage extends Component {
         return (
             <div>
                 <h1>Quantity Trumps Quality</h1>
-                <p>
-                    
+                <p className='italics'>
+                "The ceramics teacher announced on opening day that he was dividing the class into two groups. 
+                All those on the left side of the studio, he said, would be graded solely on the quantity of work they produced, all those on the right solely on its quality. 
+                His procedure was simple: on the final day of class he would bring in his bathroom scales and weigh the work of the "quantity" group: fifty pound of pots rated an "A", forty pounds a "B", and so on. 
+                Those being graded on "quality", however, needed to produce only one pot - albeit a perfect one - to get an "A".
+                Well, came grading time and a curious fact emerged: the works of highest quality were all produced by the group being graded for quantity. 
+                It seems that while the "quantity" group was busily churning out piles of work - and learning from their mistakes - the "quality" group had sat theorizing about perfection, and in the end had little more to show for their efforts than grandiose theories and a pile of dead clay." -Art and Fear by David Bayles, Ted Oraland.
+                </p>
+                <p>The team that was tasked with creating more pots, the 'quanitity' group, produced a better quality pot than the 'quality' team because instead of hyper focusing on a single product they had the chance to learn from thier mistakes. 
+                    Producing a the first pot probably had a lotof defects and simply wasn't good enough. As they went on to build the 2nd, 3rd, 30th pot, they stated to make better quality pots. 
+                    Like with coding, it is better to produce more projects, make more mistakes and learn from them.
+                    This to me personally is a great way to grow.
                 </p>
                 <Link to='/'>Home page</Link>
             </div>
